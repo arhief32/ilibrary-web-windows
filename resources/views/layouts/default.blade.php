86b2ffad4b3041f2b7505de62b876ac771eadc32
Sri Rezeki Hardiyanti <arhief32@gmail.com>
2018-09-19 20:39:23 +0700

    master

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    @include('layouts.head')
+</head>
+<body>
+    @include('layouts.header')
+
+    @yield('content')
+
+    @include('layouts.footer')
+
+    <!-- ##### jQuery (Necessary for All JavaScript Plugins) ##### -->
+    <script src="{{asset('js/jquery/jquery-2.2.4.min.js')}}"></script>
+    <!-- Popper js -->
+    <script src="{{asset('js/popper.min.js')}}"></script>
+    <!-- Bootstrap js -->
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <!-- Plugins js -->
+    <script src="{{asset('js/plugins.js')}}"></script>
+    <!-- Active js -->
+    <script src="{{asset('js/active.js')}}"></script>
+</body>
+</html>
